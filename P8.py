0c1a61e5c7d90947e740d817f60705d27e5efc33
jaiswalpalakcs252652-sketch <jaiswalpalakcs252652@gmail.com>
2026-08-27 12:41:45 +0530

    Add files via upload

---
+++ P8.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+tips = sns.load_dataset("tips")
+print(tips.head())
+sns.scatterplot(x="total_bill", y="tip", data=tips)
+plt.title("Restaurant Bill vs Tip")
+plt.xlabel("Total Bill")
+plt.ylabel("Tip")
+plt.show()
